f2476bdf507b39e450520502d544b9b22e69ce00
Charles Oertel <charles@honeybadger.net>
2011-03-31 22:06:10 +0200

    Got inserts going, now need to remove fluff around the divs

--- collapsedocument.js
+++ collapsedocument.js
@@ -1,8 +1,11 @@
 Drupal.behaviors.collapsedocument = function(context) { 
-  $('div.content h2').after('<div class="collapse">');
-  $('div.content h2').slice(1).before('</div> <!-- collapse -->');
-  if ($('div.content h2').count()) {
-    $('div.content').append('</div> <!-- collapse -->');
+  $('div.content h2').after('<div class="collapsebegin"></div>');
+  $('div.content h2').slice(1).before('<div class="collapseend"></div>');
+  if ($('div.content h2').size()) {
+    $('div.content').append('<div class="collapseend"></div>');
   }
+debugger;
+  $('div.content').val().replace('/collapsebegin"><\/div>/g', 'collapse">');
+  $('div.content').replace('/<div class="collapseend">/g', '');
 };
 
