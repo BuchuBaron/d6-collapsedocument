d58cdda2ae18c8d53abbede7f0f6ef3d0c6ce5c5
Charles Oertel <charles@honeybadger.net>
2011-04-17 10:05:28 +0200

    Fixed collapse to include only the section under the heading.  Display no longer puts an arrow next to collapsible menu items in the theme.

--- collapsedocument.js
+++ collapsedocument.js
@@ -1,7 +1,9 @@
 Drupal.behaviors.collapsedocument = function(context) { 
   $('div.content h2').addClass('expandable').after('<div class="collapse"></div>');
-  $('div.collapse').append($('div.collapse').nextUntil('h2'));
-  $('div.collapse').hide().prev('h2').toggleClass('collapsed');
+  $('div.collapse').each(function() {
+    $(this).append($(this).nextUntil('h2'));
+    $(this).hide().prev('h2').toggleClass('collapsed');
+  });
 
   $('div.content h2').click(function() {
     $(this).next('.collapse').slideToggle(600);
