9eb010343bda21e8332d7a5b13e92dd77cc4896d
Charles Oertel <charles@honeybadger.net>
2011-08-26 09:39:10 +0200

    Fixed hiding of page links and attachments at the bottom

--- collapsedocument.js
+++ collapsedocument.js
@@ -1,5 +1,14 @@
 Drupal.behaviors.collapsedocument = function(context) { 
+  // Put attachments into their own section
+  if ($('div.content h2').last().html() != "Downloads") {
+    $('table#attachments').before('<h2>Downloads</h2>');
+  }
+  // Prepare H2 for collapse
   $('div.content h2').addClass('expandable').after('<div class="collapse"></div>');
+  // Undo the Downloads h2
+  $('div.collapse').last().remove();
+  $('div.content h2').last().removeClass('expandable');
+  // Collapse the remaining headings
   $('div.collapse').each(function() {
     $(this).append($(this).nextUntil('h2'));
     $(this).hide().prev('h2').toggleClass('collapsed');
@@ -10,3 +19,20 @@ Drupal.behaviors.collapsedocument = function(context) {
     $(this).toggleClass('collapsed');
   });
 };
+
+/*
+jQuery.fn.nextUntil = function(selector) {
+  var query = jQuery([]);
+  while (true) {
+    var next = this.next();
+    if (next.length == 0 || next.is(selector)) {
+      return query;
+    }
+    query.add(next);
+  }
+  return query;
+}
+
+// To retrieve all LIs avec a parent
+$(".parent:first").nextUntil(".parent");
+*/
