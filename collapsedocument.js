00bbd07b5b7f809879cbe967f601ce26c80ce1ba
Charles Oertel <charles@honeybadger.net>
2011-09-04 08:43:33 +0200

    Bugfixes in dealing with the last Downloads header

--- collapsedocument.js
+++ collapsedocument.js
@@ -1,13 +1,15 @@
 Drupal.behaviors.collapsedocument = function(context) { 
   // Put attachments into their own section
   if ($('div.content h2').last().html() != "Downloads") {
-    $('table#attachments').before('<h2 id="Downloads">Downloads</h2>');
+    $('table#attachments').before('<h2>Downloads</h2>');
   }
+  $('div.content h2').last().attr('Id', 'Downloads');
+  
   // Prepare H2 for collapse
   $('div.content h2').addClass('expandable').after('<div class="collapse"></div>');
   // Undo the Downloads h2
-  $('h2#Downloads>div.collapse').remove();
-  $('div.content h2#Downloads').removeClass('expandable');
+  $('h2#Downloads').removeClass('expandable');
+  $('h2#Downloads').next().remove();
   // Collapse the remaining headings
   $('div.collapse').each(function() {
     $(this).append($(this).nextUntil('h2'));
